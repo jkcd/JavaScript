fcd77c70599054ae6bf398bb39faea0957c6a0bc
jramamoorthy <jramamoorthy@paypal.com>
2017-11-05 16:30:47 +0530

    initital commit

---
+++ sampleReactApp/server/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by jramamoorthy on 05/11/17.
+ */
+
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send({hi: 'I am right here'});
+});
+
+// read dynamic port value from environment variable
+const PORT = process.env.port || 5555;
+
+app.listen(5555);
